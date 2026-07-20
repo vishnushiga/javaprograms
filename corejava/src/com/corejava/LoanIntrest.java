4dcefce7f5f1c82a47767016e19f7c5e92f887d1
Vishnu Shiga <vishnushiga2829@gmail.com>
2026-07-20 17:44:16 +0530

    Intrest Calculation

---
+++ corejava/src/com/corejava/LoanIntrest.java
@@ -0,0 +1,26 @@
+package com.corejava;
+import java.util.Scanner;
+
+public class LoanIntrest {
+	double CalculateIntrest(double principle,double rate,int time) {
+		double simpleintrest = ((principle*rate*time)/100);
+		System.out.println("Total Bank Loan Intrest :" +simpleintrest);
+		return simpleintrest;
+	}
+
+	 void main() {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter principle amount :" );
+		 double principle =sc.nextDouble();
+		
+		 System.out.println("Enter Intrest rate :");
+		 double rate = sc.nextDouble();
+		 
+		 System.out.println("Enter time :");
+		 int time = sc.nextInt();
+		  CalculateIntrest(principle,rate,time);
+
+		 
+	}
+
+}
