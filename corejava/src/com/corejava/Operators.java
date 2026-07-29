ea7c24aac490e47b8e545a6e8786ca3eedc9ffcb
Vishnu Shiga <vishnushiga2829@gmail.com>
2026-07-29 21:45:06 +0530

    Operators

---
+++ corejava/src/com/corejava/Operators.java
@@ -0,0 +1,40 @@
+package com.corejava;
+
+public class Operators {
+	int a=10;
+	int b=20;
+	int c=30;
+	int d=40;
+	int e;
+	int f;
+	int g;
+	int h;
+
+	 void main(String[] args) {
+//		 int c = a+b;
+//		 int d = a-b;
+//		 int e = a*b;
+//		 int f = a/b;
+//		 int g = a%b;
+		 System.out.println("******Arithematic Operations*******");
+		 System.out.println("Addition of Two Numbers       :"+(a+b));
+		 System.out.println("Subtraction of Two Numbers    :"+(a-b));
+		 System.out.println("Multiplication of Two Numbers :"+(a*b));
+		 System.out.println("Division of Two Numbers       :"+(a/b));
+		 System.out.println("Modulus of two Numbers        :"+(a%b));
+		 System.out.println("------------------------------------");
+//	Unary Operators i) Increment
+//		  e = a++;
+//		  f = ++b;
+////		            ii) Decrement
+//		  g = c--;
+//		  h = --d;
+		  System.out.println("*****Unary Operators******");
+		  System.out.println("Unary Increment a :"+(a++));
+		  System.out.println("Unary Increment b :"+(++b));
+		  System.out.println("Unary Decrement c :"+(--c));
+		  System.out.println("Unary Decrement d :"+(d--));
+
+	}
+
+}
