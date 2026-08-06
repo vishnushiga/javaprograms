859bbca10e34989bb96c12a21a722a9dc74df978
Vishnu Shiga <vishnushiga2829@gmail.com>
2026-08-06 21:46:12 +0530

    For Looping

---
+++ corejava/src/logicalstatements/Reverseorder.java
@@ -0,0 +1,44 @@
+package logicalstatements;
+
+public class Reverseorder {
+	 static int sum=0;
+//	print even numbers
+	
+		for(int i=1;i<=100;i++) {
+			if(i%2==0) {
+				System.out.print(i+" ");
+			}	
+		
+
+	 void main(String[] args) {
+	
+	
+}
+		 
+//		 Sum of first N numbers.
+		 
+		 
+			 
+			 for(int i=0;i<=100;i++) {
+				 sum +=i;
+			 }
+			 System.out.print(sum);
+		 
+		 
+		 
+		 
+//		 Sum of even numbers.
+		 void 
+//		 int sum =0;
+//		 for(int i=0;i<=100;i++) {
+//			 if(i%2==0) {
+//				 sum += i;
+//				 
+//			 }
+//			 
+//		 }
+//		 System.out.print(sum+" ");
+		 
+		 
+	 }
+}
---
+++ corejava/src/logicalstatements/SumOfSquares.java
@@ -0,0 +1,22 @@
+package logicalstatements;
+
+public class SumOfSquares {
+
+	 void main(String[] args) {
+		 Cube();
+		 int sum =0;
+		 for(int i=5;i<=10;i++) {
+			 int sqr = i*i;
+			 sum += sqr; 
+		 }
+		 System.out.println(sum);
+	}
+	 void Cube() {
+		 int sum = 0;
+		 for(int i=0;i<=10;i++) {
+			 sum +=(i*i*i);
+		 }
+		 System.out.println(sum);
+	 }
+
+}
